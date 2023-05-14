c6647f03f9130d9301e041b0203d8ce37fcba620
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-05-14 14:47:29 +0900

    OpenAI ChatGPTをAPI実装

--- common/api.go
+++ common/api.go
@@ -1,24 +1,22 @@
 package common
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	// "github.com/joho/godotenv"
+	"github.com/sashabaranov/go-openai"
 	"github.com/trailstem/chatbot-server/domain"
 )
 
 // OpenWeatherAPIを使用して天気情報を取得する
 func GetWeather() (string, error) {
-	// err := godotenv.Load(".env")
-
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-
+	// .envファイルを読み込む
 	wk := os.Getenv("WEATHER_KEY")
 
 	//都市「東京」、言語「日本語」、温度単位「`C」に設定
@@ -45,4 +43,37 @@ func GetWeather() (string, error) {
 	description := weatherData.Weather[0].Description
 	fmt.Println(description)
 	return description, nil
+}
+
+// OpenAI ChatGPTを使用してチャットボットとのやりとりを行う
+func ChatGPT(userInput string) (string, error) {
+
+	// envからOPEN_API取得
+	oiKey := os.Getenv("OPEN_API")
+	client := openai.NewClient(oiKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: userInput,
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return "", err
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	resText := resp.Choices[0].Message.Content
+	//画面に表示しやすいように「.」「。」で改行追加
+	replacer := strings.NewReplacer(
+		"。", "。\n",
+	)
+	resT := replacer.Replace(resText)
+	return resT, nil
 }
