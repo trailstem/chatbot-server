bb6f9b36ea8a0a468e6f738d4931e8f017b4c52d
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-05-14 21:38:33 +0900

    time.Time型をstringに変更

--- domain/history_list.go
+++ domain/history_list.go
@@ -1,12 +1,10 @@
 package domain
 
-import "time"
-
 type HistoryList struct {
-	ID                int       `json:"id"`
-	UserInput         string    `json:"user_input"`
-	BotResponse       string    `json:"bot_response"`
-	ResponseTimestamp time.Time `json:"response_timestamp"`
+	ID                int    `json:"id"`
+	UserInput         string `json:"user_input"`
+	BotResponse       string `json:"bot_response"`
+	ResponseTimestamp string `json:"response_timestamp"`
 }
 
 type WeatherData struct {
